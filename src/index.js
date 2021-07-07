bd3fbb98350d5675fe24d3c4d4343909d6c97e31
WycliffeMuchumi <muchumimaina94@gmail.com>
2021-07-07 03:27:03 -0700

    Setting up environment

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// ReactDom.render takes in two arguments, what I want to render and where I want to render it i.e("root")
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+ReactDOM.render(<div><h1>Unordered list</h1>
+<ul>
+  <li>Great</li>
+  <li>Amazing</li>
+  <li>Super</li>
+  <li>Excellent</li>
+</ul>
+  </div>, document.getElementById("root"));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
