a1af8aa5ea1ed53013812f4fe544213e5d1e7fb9
WycliffeMuchumi <muchumimaina94@gmail.com>
2021-07-07 04:58:23 -0700

    setting project structure

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+// import logo from '../src/logo.svg';
+// import '../src/Home.css';
+
+// Function component
+// Returns JSX code
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <h1>Welcome To Playground</h1>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default Home;
+
+
